cdd782c036314d4b3a4b9ae02863aabcf747802c
fengzipei <fengzipei3@gmail.com>
2017-05-02 14:38:55 +0800

    change from gbk to utf8 & reformat code & fix a bug

---
+++ cmake-build-debug/CMakeFiles/SSDsim_origin.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/avlTree.c" "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/cmake-build-debug/CMakeFiles/SSDsim_origin.dir/avlTree.c.o"
+  "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/flash.c" "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/cmake-build-debug/CMakeFiles/SSDsim_origin.dir/flash.c.o"
+  "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/initialize.c" "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/cmake-build-debug/CMakeFiles/SSDsim_origin.dir/initialize.c.o"
+  "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/pagemap.c" "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/cmake-build-debug/CMakeFiles/SSDsim_origin.dir/pagemap.c.o"
+  "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/ssd.c" "/cygdrive/c/Users/lab/CLionProjects/SSDsim_origin/cmake-build-debug/CMakeFiles/SSDsim_origin.dir/ssd.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SSDsim_origin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SSDsim_origin.dir/avlTree.c.o"
+  "CMakeFiles/SSDsim_origin.dir/flash.c.o"
+  "CMakeFiles/SSDsim_origin.dir/initialize.c.o"
+  "CMakeFiles/SSDsim_origin.dir/pagemap.c.o"
+  "CMakeFiles/SSDsim_origin.dir/ssd.c.o"
+  "SSDsim_origin.pdb"
+  "SSDsim_origin.exe"
+  "libSSDsim_origin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SSDsim_origin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
